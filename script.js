fca9c18a260f3c77960a9daeb6860e16f07434f3
Niklas T <niklas.tibbe@gmail.com>
2023-02-27 10:59:50 +0100

    bottomcard_navbar_added

--- script.js
+++ script.js
@@ -46,16 +46,25 @@ function getType(currentPokemon, i, ID) {
   }
 }
 
-// Rendering the popUpCart
 
+// Rendering the popUpCart
 function openPokeCard(i) {
   document.getElementById("popUpContainer").classList.remove("dnone");
   let pokecard = document.getElementById("openPokeCard");
   let bigPokemon = allPokemon[i - 1];
   let fightclasstype = bigPokemon.types[0].type.name;
-  pokecard.innerHTML = generatePokecard(bigPokemon, fightclasstype);
+  pokecard.innerHTML = generatePokecard(bigPokemon, fightclasstype, i);
   getType(bigPokemon, i, "pokemonCardClass");
+  prepareAboutCategory(i);
   renderAbilities(bigPokemon);
+}
+
+function closePokeCard() {
+  document.getElementById("popUpContainer").classList.add("dnone");
+}
+
+function doNotClose(event) {
+  event.stopPropagation();
 }
 
 function renderAbilities(bigPokemon) {
@@ -80,15 +89,7 @@ function createAbilitiesWithoutComma(newAbility) {
   return `${newAbility.ability.name}`;
 }
 
-function closePokeCard() {
-  document.getElementById("popUpContainer").classList.add("dnone");
-}
-
-function doNotClose(event) {
-  event.stopPropagation();
-}
-
-function generatePokecard(bigPokemon, fightclasstype) {
+function generatePokecard(bigPokemon, fightclasstype, i) {
   return `
     <div class="openPokeCardTop ${fightclasstype}">
     <div class="openPokemonCardHeader">
@@ -105,36 +106,96 @@ function generatePokecard(bigPokemon, fightclasstype) {
 
   <div class="openPokeCardBottom">
     <div class="categoryHeader">
-      <p>About</p>
-      <p>Base Stats</p>
-      <p>Moves</p>
+      <p onclick="prepareAboutCategory(${i})"class="category">About</p>
+      <p onclick="prepareBaseStats(${i})" class="category">Base Stats</p>
+      <p onclick="prepareMoves(${i})"class="category">Moves</p>
     </div>
-    <div class="categories">
-      <div class="categoryAbout">
-        <div class="attribute">
-          <p>Height:</p>
-          <p>${(bigPokemon.height / 10).toFixed(1).replaceAll(".", ",")} m</p>
-        </div>
-        <hr class="line" />
-        <div class="attribute">
-          <p>Weight:</p>
-          <p>${(bigPokemon.weight / 10).toFixed(1).replaceAll(".", ",")} kg</p>
-        </div>
-        <hr class="line" />
-        <div class="attribute">
-          <p>Abilities:</p>
-          <p id=abilities></p>
-        </div>
-        <hr class="line" />
-        <div class="attribute">
-          <p>Base Experience</p>
-          <p>${bigPokemon.base_experience}</p>
-        </div>
-        <hr class="line" />
-      </div>
-      <div class="categoryStats"></div>
-      <div class="categoryMoves"></div>
+    <div class="categories" id="categories">
+
     </div>
   </div>
      `;
+}
+
+
+function prepareAboutCategory(i){
+  let currentPokemon = allPokemon[i-1];
+  let categoryAbout = document.getElementById("categories");
+  categoryAbout.innerHTML = ``;
+  categoryAbout.innerHTML = renderAboutCategory(currentPokemon, i);
+  renderAbilities(currentPokemon);
+}
+
+function renderAboutCategory(currentPokemon,i){
+  return `<div class="categoryAbout">
+  <div class="attribute">
+    <p>Height:</p>
+    <p>${(currentPokemon.height / 10).toFixed(1).replaceAll(".", ",")} m</p>
+  </div>
+  <hr class="line" />
+  <div class="attribute">
+    <p>Weight:</p>
+    <p>${(currentPokemon.weight / 10).toFixed(1).replaceAll(".", ",")} kg</p>
+  </div>
+  <hr class="line" />
+  <div class="attribute">
+    <p>Abilities:</p>
+    <p id="abilities"></p>
+  </div>
+  <hr class="line" />
+  <div class="attribute">
+    <p>Base Experience</p>
+    <p>${currentPokemon.base_experience}</p>
+  </div>
+  <hr class="line" />
+</div>`;
+}
+
+
+function prepareBaseStats(i){
+  let currentPokemon = allPokemon[i];
+  let baseStatsCategory = document.getElementById("categories");
+  baseStatsCategory.innerHTML = ``;
+  baseStatsCategory.innerHTML = renderBaseStats(currentPokemon, i);
+}
+
+function renderBaseStats(currentPokemon,i){
+  return `
+  <div class="stats">
+  <div class="progress" role="progressbar" aria-label="Example with label">
+  <div class="progress-bar bg-success" style="width: 25%">HP</div>
+  </div>
+
+  <div class="progress" role="progressbar">
+  <div class="progress-bar bg-danger" style="width: 50%">Attack</div>
+  </div>
+
+  <div class="progress" role="progressbar">
+  <div class="progress-bar bg-warning" style="width: 75%">Defense</div>
+  </div>
+
+  <div class="progress" role="progressbar">
+  <div class="progress-bar bg-info" style="width: 100%">Special-Attack</div>
+  </div>
+
+  <div class="progress" role="progressbar">
+  <div class="progress-bar" style="width: 100%">Special-Defense</div>
+  </div>
+  
+  <div class="progress" role="progressbar">
+  <div class="progress-bar bg-purple" style="width: 100%"> Speed</div>
+  </div>
+  </div>`;
+}
+  
+
+function prepareMoves(i){
+  let currentPokemon = allPokemon[i];
+  let movesCategory = document.getElementById("categories");
+  movesCategory.innerHTML = ``;
+  movesCategory.innerHTML = renderMoves(currentPokemon, i);
+}
+
+function renderrenderMoves(currentPokemon, i){
+  return ``;
 }
